65c0d9a214da1af400f23a72def2876531fe5146
Budi Setiawan <bud1.setia1@gmail.com>
2020-04-30 06:47:09 +0700

    init deployHq handler

--- routes/notification.js
+++ routes/notification.js
@@ -18,13 +18,67 @@ router.post('/', function(req, res, next) {
     && login === auth.login
     && password === auth.password
   ) {
-    var data = JSON.stringify(req.body);
-    console.log(data);
-    return res.end(data);
+    // TODO: ensure for base64-encoded signature from Deploy
+    var data = req.body;
+    // var data = dummyData;
+    
+    // console.log(JSON.stringify(data));
+    deployHqHandler(data);
+
+    return res.end('OK');
   }
 
   // Access denied...
   res.status(401).send('Authentication failure.')
 });
+
+// deployhq handler
+function deployHqHandler(req) {
+  var payload = JSON.parse(req.payload);
+  var {
+    identifier,
+    servers,
+    project: {
+      name: projectName,
+    },
+    deployer,
+    branch,
+    start_revision: { ref: startRev },
+    end_revision: { ref: endRev },
+    status,
+    timestamps: {
+      queued_at,
+      started_at,
+      completed_at,
+      duration,
+    },
+  } = payload;
+
+  // TODO: get server name
+  // name, last_revision, auto_deploy
+
+  if (req.payload) {
+    var deployType = 'Manual Deployment';
+    var serverNames = [];
+
+    servers.forEach(function(server) {
+      if (server.auto_deploy) {
+        deployType = 'Auto Deployment';
+      }
+
+      if (server.name) {
+        serverNames.push(`(*) ${server.name} (last revision: ${server.last_revision})`);
+      }
+    });
+
+    // TODO: beatufiy with new line
+    var message = `>${deployType}: *${projectName}* (identifier: ${identifier}) *@${branch} on: ${serverNames.join(', ')}* is *${status}*. Start revision ${startRev} : End revision ${endRev}. By ${deployer}.`;
+    console.log(message);
+  }
+}
+
+function sendToSlack() {
+
+}
 
 module.exports = router;
