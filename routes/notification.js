b3fd8185944019f6d734e1f63e2ebf0417c18a2c
Budi Setiawan <bud1.setia1@gmail.com>
2020-04-29 22:47:13 +0700

    setup new module (notification) & environment

---
+++ routes/notification.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/**
+ * currently only available for slack
+ */
+router.post('/', function(req, res, next) {
+  var auth = {
+    login: process.env.USERNAME,
+    password: process.env.PASSWORD,
+  };
+
+  var b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  var [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+
+  if (login
+    && password
+    && login === auth.login
+    && password === auth.password
+  ) {
+    res.send(res.body);
+  }
+
+  // Access denied...
+  res.status(401).send('Authentication failure.')
+});
+
+module.exports = router;
